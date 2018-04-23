675af20c393cef35f4b7ee3e33f4df7e67b4501c
cs.yi <1580325417@qq.com>
2018-04-23 09:33:29 +0800

    kafka producer and consumer

--- run.py
+++ run.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-	return render_template("index.html")
-	
-if __name__ == '__main__':
-	app.run()
+    return render_template("index.html")
 
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", degug=True)
