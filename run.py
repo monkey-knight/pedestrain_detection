95fdb4f813bca1c6ed39e2b81ffa97b44f7d744e
cs.yi <1580325417@qq.com>
2018-05-03 23:51:20 +0800

    Add video html

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
+import cv2
+import os
 
 app = Flask(__name__)
 
@@ -7,6 +9,16 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/video")
+def video():
+    images = []
+    img_dir = "./static/images"
+    for file in os.listdir(img_dir):
+        image = img_dir + "/" + file
+        images.append(image)
+    return render_template("video.html", images=images)
 
 
 if __name__ == '__main__':
